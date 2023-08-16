543b8a6b60ea7977ff69162611d0256808c38c6c
lucjastozek <lucjastozek@gmail.com>
2023-08-16 15:28:21 +0200

    adjust getfontsize function for shorter words

--- src/utils/getFontSize.ts
+++ src/utils/getFontSize.ts
@@ -1,9 +1,13 @@
 function getFontSize(sentence: string) {
   const words = sentence.split(" ");
   words.sort((a, b) => b.length - a.length);
-  return {
-    fontSize: `${50 / words[0].length}vw`,
-  };
+  return words[0].length > 3
+    ? {
+        fontSize: `${50 / words[0].length}vw`,
+      }
+    : {
+        fontSize: `20vh`,
+      };
 }
 
 export default getFontSize;
